d9f423224e2c4bd01ecc28ad1328930b2f3b94d9
Daniel <danylo.g@smart-gamma>
2022-11-15 11:51:26 +0100

    Blocks foryou results youfind + adaptation

--- js/script.js
+++ js/script.js
@@ -5,4 +5,15 @@ window.onload = function () {
 		easing: 'ease-in-quad',
 		// delay: 100,
 	});
+
+	$('.results-slider').slick({
+		draggable: false,
+		dots: true,
+		prevArrow: $('.arrow-left'),
+		nextArrow: $('.arrow-right'),
+		dotsClass: 'results-slider__dots',
+		autoplay: true,
+  	autoplaySpeed: 5000,
+		adaptiveHeight: true
+	});
 }
